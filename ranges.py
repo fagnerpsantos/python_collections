1b362971a55bb18325a7209a2ef959c669bc3f69
Fagner Pinheiro <=>
2018-09-27 14:33:15 -0300

    Aulas de ranges e tuplas

---
+++ ranges.py
@@ -0,0 +1,10 @@
+#Declarando um range
+
+for i in range(5):
+    print(i)
+
+for i in range(3, 6):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
---
+++ tuplas.py
@@ -0,0 +1,16 @@
+# Métodos count()  e index() - Vídeo
+
+# Criando uma tupla
+
+minha_tupla = (1, 1, 3, 4, "Olá, mundo")
+print(minha_tupla)
+
+#Método count()
+print(minha_tupla.count(1))
+
+#Método index()
+print(minha_tupla.index(3))
+
+#Verificar existência de elemento na tupla
+print(13 in minha_tupla)
+
