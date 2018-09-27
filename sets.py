d3c0c832ad18fd44704940af41a9309423d3c075
Fagner Pinheiro <=>
2018-09-27 15:06:47 -0300

    Sets, frozensets e sets comprehensions pronto

--- sets.py
+++ sets.py
@@ -21,6 +21,7 @@ meu_set_3 = {1, 2}
 # print(meu_set_3)
 #meu_set_3.remove(3)
 
+#Unions, intersections...
 meu_set_4 = {2, 4}
 print(meu_set_3 | meu_set_4)
 print(meu_set_3.union(meu_set_4))
@@ -33,3 +34,15 @@ print(meu_set_3.difference(meu_set_4))
 
 print(meu_set_3 ^ meu_set_4)
 print(meu_set_3.symmetric_difference(meu_set_4))
+
+#Set Comprehensions
+meu_set_5 = {i for i in meu_set_3.union(meu_set_4)}
+print(f"Meu set {meu_set_5}")
+
+#Frozen Sets
+frozen_set_1 = frozenset([1, 2, 3, 4])
+frozen_set_2 = frozenset([1, 2, 5, 6])
+print(frozen_set_1)
+print(frozen_set_2)
+print(frozen_set_1.union(frozen_set_2))
+frozen_set_1.add(4)
